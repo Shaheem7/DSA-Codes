373fd3bfacd01f5d75b9045e43e8840685d5ba97
Shaheem7 <shaheembutt11@gmail.com>
2023-12-29 23:11:27 +0530

    adding to code

--- 3_Array/P8_Triplets_with_given_sum.py
+++ 3_Array/P8_Triplets_with_given_sum.py
@@ -4,31 +4,58 @@ def three_sum(nums):
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             for k in range(j+1, len(nums)):
-                if i != j and i != k  and j != k and (nums[i] + nums[j] + nums[k] == 0):
-                    # print(f'{nums[i]},{nums[j]},{nums[k]}')
-                    ans.append([nums[i],nums[j],nums[k]])
-              
-    
-    # print(ans)
-    # print(sorted(ans))
-    
-    temp = []
-    for e in ans:
-        e.sort()
-        # print(e)
-        temp.append(e)
-        
-    for i in range(len(temp)-1):
-        for j in range(i+1,len(temp)):
-            if temp[i] == temp[j]:
-                temp.pop(i)
+                if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0:
+                    temp = [nums[i],nums[j],nums[k]]
+                    ans.append(temp)
             
-    print(temp)
+                
+    print(ans)
+    return ans
+    
 nums = [-1,0,1,2,-1,-4]
+
 s = 5
 
 three_sum(nums)
 
-                    # if nums[i] + nums[j] == s:
-                    #     temp = [min(nums[i], nums[j]), max(nums[i], nums[j])]
-                    #     ans.append(temp)
+
+# Chatjpt 
+
+def three_sum(nums):
+    nums.sort()
+    result = []
+
+    for i in range(len(nums) - 2):
+        # Skip duplicates
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        left = i + 1
+        right = len(nums) - 1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+
+            if total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
+
+                # Skip duplicates
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+    return result
+
+# Example usage:
+nums = [-1, 0, 1, 2, -1, -4]
+distinct_triplets = three_sum(nums)
+print("Distinct triplets with sum 0:", distinct_triplets)
+
